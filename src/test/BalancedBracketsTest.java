895d0a4ee21f3863102b67f6022ca78f2b5d1588
Anthony Luong <anthony@Anthonys-MacBook-Pro.local>
2023-01-25 17:19:25 -0500

    Studio Done

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,48 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
-
-
+    @Test
+    public void bothBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void onlyOneLeftBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyOneRightBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void afterBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Mary had a little lamb []"));
+    }
+    @Test
+    public void beforeBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Mary had a little lamb"));
+    }
+    @Test
+    public void insideBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Mary [had] a little lamb"));
+    }
+    @Test
+    public void doubleBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Mary] had [a] little lamb"));
+    }
+    @Test
+    public void tripleBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Mary] had [a] little [lamb]"));
+    }
+    @Test
+    public void oppositeTwoBracketReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Mary ]had ]a little lamb"));
+    }
+    @Test
+    public void wrongDirectionBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Mary[ had a little lamb"));
+    }
+    @Test
+    public void oppositeDirectionBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
 }
